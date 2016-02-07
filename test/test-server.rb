526a16140074af4fccb0290b03ea08ba51603ef7
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-02-07 19:57:41 +0900

    Start unit testing

---
+++ test/test-server.rb
@@ -0,0 +1,22 @@
+require "feedcellar/test/server"
+require "faraday"
+require "rexml/document"
+
+class ServerTest < Test::Unit::TestCase
+  def setup
+    @server = Feedcellar::Test::Server.new
+    @server.start
+  end
+
+  def teardown
+    @server.stop
+  end
+
+  def test_rss_version
+    connection = Faraday::Connection.new(url: "http://localhost:20075")
+    response = connection.get("/feed.xml")
+    doc = REXML::Document.new(response.body)
+    rss_version = doc.elements["rss"].attributes["version"]
+    assert_equal("2.0", rss_version)
+  end
+end
