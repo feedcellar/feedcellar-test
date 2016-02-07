319a2919092a85bde00a375bd7c0e37c18371af2
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-02-07 17:20:31 +0900

    Start
        % bundle gem feedcellar-test

---
+++ lib/feedcellar/test.rb
@@ -0,0 +1,7 @@
+require "feedcellar/test/version"
+
+module Feedcellar
+  module Test
+    # Your code goes here...
+  end
+end
---
+++ lib/feedcellar/test/version.rb
@@ -0,0 +1,5 @@
+module Feedcellar
+  module Test
+    VERSION = "0.1.0"
+  end
+end
